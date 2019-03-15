d2f68c33f37bdc137a7964d3a65cd6c0cd96aa55
quinhill@icloud.com <quinhill@icloud.com>
2019-03-15 12:15:03 -0600

    Add withRouter to navbar component to be able to toggle active class on NavLinks

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,28 +1,23 @@
 const authReducer = (state = {}, action) => {
   switch(action.type) {
     case 'LOGIN_ERROR':
-      console.log(action.err)
       return {
         ...state,
         authError: action.err.message
       }
     case 'LOGIN_SUCCESS':
-      console.log('login success')
       return {
         ...state,
         authError: null
       }
     case 'SIGNOUT_SUCCESS':
-      console.log('signout success');
       return state
     case 'SIGNUP_SUCCESS':
-      console.log('signup success')
       return {
         ...state,
         authError: null
       }
       case 'SIGNUP_ERROR':
-      console.log('signup error')
       return {
         ...state,
         authError: action.err.message
