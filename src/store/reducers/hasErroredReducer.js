f2b00b2e182013ddb6a712b3782f73e0da8e89ed
quinhill@icloud.com <quinhill@icloud.com>
2019-06-16 07:48:36 -0600

    Add error message for failed login

--- src/store/reducers/hasErroredReducer.js
+++ src/store/reducers/hasErroredReducer.js
@@ -1,6 +1,7 @@
 const hasErrored = (state = '', action) => {
   switch(action.type) {
     case 'HAS_ERRORED':
+      console.log(action.message)
       return action.message
     default:
       return state;
